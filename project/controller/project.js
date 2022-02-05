87a3c42b535a5cdff04c4d9870b17d0ab8ad44c5
faeshal_ <faeshal@faeshals-MacBook-Pro.local>
2022-02-05 14:57:56 +0700

    start prepare notif services

--- project/controller/project.js
+++ project/controller/project.js
@@ -2,6 +2,7 @@ require("pretty-error").start();
 const asyncHandler = require("express-async-handler");
 const log = require("log4js").getLogger("avatar");
 log.level = "info";
+const createProject = require("../publisher/createProject");
 
 // * @route POST /api/v1/projects
 // @desc    post dummy
@@ -13,9 +14,9 @@ exports.createProject = asyncHandler(async (req, res, next) => {
     projectName: Math.random().toString(36).substring(3, 9),
     createdAt: new Date().getTime(),
   };
-  log.debug("project", project);
 
   // * send to publisher
+  await createProject(project);
 
   res.status(201).json({
     success: true,
